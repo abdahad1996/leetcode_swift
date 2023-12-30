7f52864d655b3cc948b98521706836fd0199b139
abdul ahad <abdul_ahad1996@live.com>
2023-12-30 20:32:28 +0100

    Time: 157 ms (5.18%), Space: 16.6 MB (7.62%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.swift
@@ -0,0 +1,33 @@
+
+class MinStack {
+
+    var arr:[Int] = []
+    init() {
+        
+    }
+    
+    func push(_ val: Int) {
+        arr.append(val)
+    }
+    
+    func pop() {
+        arr.removeLast()
+    }
+    
+    func top() -> Int {
+        return arr[arr.count-1] 
+    }
+    
+    func getMin() -> Int {
+        return arr.min()! 
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * let obj = MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * let ret_3: Int = obj.top()
+ * let ret_4: Int = obj.getMin()
+ */
