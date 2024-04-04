f7194bae6dd0bab49efe015670e2b8936788f13f
abdul ahad <abdul_ahad1996@live.com>
2024-04-04 02:37:44 +0200

    Time: 23 ms (58.02%), Space: 16.3 MB (20.77%) - LeetHub

--- 0001-two-sum/0001-two-sum.swift
+++ 0001-two-sum/0001-two-sum.swift
@@ -2,7 +2,6 @@ class Solution {
     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
        //        //[9:0,1:1]
        var numDic = [Int:Int]()
-//        let arr = uniqueArr
        var count = 0
        
        for (index,num) in nums.enumerated(){
@@ -20,29 +19,7 @@ class Solution {
            numDic[num] = index
           
            
-           
        }
-       
-        
-        var leftPointer = 0
-//         var rightpointer = nums.count-1
-//         // var count = 0
-// //        [1,9,2,8,7,3] = 10
-//         while(leftPointer < nums.count-1 || rightpointer > 0){
-//             if nums[leftPointer] + nums[rightpointer] == target {
-//                 print("pairs,\(nums[leftPointer]),\(nums[rightpointer])")
-//                 // count = count + 1
-//                 return [leftPointer,rightpointer]
-//             }
-//             else if nums[leftPointer] + nums[rightpointer] > target {
-//                 rightpointer = rightpointer - 1
-//             }
-            
-//             else if nums[leftPointer] + nums[rightpointer] < target {
-//                 leftPointer = leftPointer + 1
-//             }
-//         }
-        
         
         return []
     }
