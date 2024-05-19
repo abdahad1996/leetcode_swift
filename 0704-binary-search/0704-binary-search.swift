09e979212c688a97ba3ce3f45328cfb33583bdef
abdul ahad <abdul_ahad1996@live.com>
2024-05-19 16:35:42 +0200

    Time: 104 ms (40.85%), Space: 16.1 MB (9.06%) - LeetHub

--- 0704-binary-search/0704-binary-search.swift
+++ 0704-binary-search/0704-binary-search.swift
@@ -1,20 +1,26 @@
 class Solution {
+    //left and right pointer
+    //mid point index
+    //target < mid value , right pointer will mid - 1
+    //target > mid value , left pointer will mid +1
+
     func search(_ nums: [Int], _ target: Int) -> Int {
-        var left = 0
-        var right = nums.count - 1
         
-        while(left<=right) {
+        var left = 0
+        var right = nums.count-1
+        
+        while left <= right {
             var mid = (right+left)/2
-            
-            if target < nums[mid] {
-                right = mid - 1
-            }else if target > nums[mid] {
-                left = mid + 1
+            print(mid)
+            if (target < nums[mid]) {
+                right = mid-1
+            }else if (target > nums[mid]) {
+                left = mid+1
             }
             else{
                 return mid
             }
         }
         return -1
-        }
+    }
 }
