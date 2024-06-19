22fef228ff608babd08d49f42088e1f022352ce9
abdul ahad <abdul_ahad1996@live.com>
2024-06-19 21:52:18 +0200

    Time: 545 ms (54.81%), Space: 22.5 MB (63.46%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.swift
+++ 0912-sort-an-array/0912-sort-an-array.swift
@@ -1,30 +1,51 @@
-//inserton sort 
-// Insertion Sort Works
-// Start with the second element (assuming the first element is a sorted sublist of one element).
-// Compare the current element to the elements in the sorted sublist (to its left).
-// Shift all elements in the sorted sublist that are greater than the current element to the right.
-// Insert the current element into its correct position in the sorted sublist.
-// Repeat steps 2-4 for each element until the entire array is sorted.
-
 class Solution {
+    //merge sort 
+    //divide and merge
     func sortArray(_ nums: [Int]) -> [Int] {
-         var arr = nums
-        if arr.count == 1 {
-            return arr
-        }
-        for i in 1...nums.count-1 {
-            
-            var j = i-1
-            while ( j >= 0 && arr[j+1] < arr[j]) {
-                var temp = arr[j+1]
-                arr[j+1] = arr[j]
-                arr[j] = temp 
-                
-                j = j - 1
-            }
-            
-        }
+        //base case
+        guard nums.count > 1 else {return nums}
         
-        return arr
+        let middleIndex = nums.count/2
+        var leftArr = Array(nums[0..<middleIndex])
+        var rightArr = Array(nums[middleIndex..<nums.count])
+        
+        //recursivelySORT
+        var sortedLeftArray = sortArray(leftArr)
+        var sortedRightArray = sortArray(rightArr)
+        
+        return merge(sortedLeftArray, sortedRightArray)
+
     }
+}
+
+func merge(_ left: [Int], _ right: [Int]) -> [Int] {
+    var leftIndex = 0
+    var rightIndex = 0
+    var sortedArr = [Int]()
+    
+    // Compare elements from both arrays and merge them in sorted order
+    while(leftIndex < left.count && rightIndex < right.count){
+        if left[leftIndex] < right[rightIndex]{
+            sortedArr.append(left[leftIndex])
+            leftIndex += 1
+        }else{
+            sortedArr.append(right[rightIndex])
+            rightIndex += 1
+        }
+    }
+    
+    // If there are remaining elements in the left array, add them to the result
+    while leftIndex < left.count {
+        sortedArr.append(left[leftIndex])
+        leftIndex += 1
+    }
+    
+    // If there are remaining elements in the right array, add them to the result
+    while rightIndex < right.count {
+        sortedArr.append(right[rightIndex])
+        rightIndex += 1
+    }
+    
+    return sortedArr
+    
 }
